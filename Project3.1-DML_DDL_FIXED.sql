fc9dcbedd6f07fc6f8cb1f5e7e938e6edb55dee7
garyni1693 <164397461+garyni1693@users.noreply.github.com>
2024-04-12 01:18:25 -0400

    Update Project3.1-DML_DDL_FIXED.sql

--- Project3.1-DML_DDL_FIXED.sql
+++ Project3.1-DML_DDL_FIXED.sql
@@ -1,3 +1,49 @@
+/*
+--user
+GRANT SELECT ON Shows TO reg_use;
+GRANT SELECT ON Director TO reg_use;
+GRANT SELECT ON Actor TO reg_use;
+GRANT SELECT ON Genre TO reg_use;
+GRANT SELECT ON Type TO reg_use;
+GRANT EXECUTE ON app_admin.update_customer_profile TO reg_use;
+GRANT EXECUTE ON app_admin.insert_new_customer TO reg_use;
+GRANT EXECUTE ON app_admin.InsertActorRating TO reg_use;
+GRANT EXECUTE ON app_admin.InsertDirectorRating TO reg_use;
+GRANT EXECUTE ON app_admin.InsertGenreRating TO reg_use;
+GRANT EXECUTE ON app_admin.InsertProviderRating TO reg_use;
+GRANT EXECUTE ON app_admin.InsertShowRating TO reg_use;
+GRANT EXECUTE ON app_admin.get_popular_shows TO reg_use;
+GRANT EXECUTE ON app_admin.PurchaseSubscriptionPlan TO reg_use;
+GRANT SELECT ON combined_affection_ratings TO reg_use;
+
+
+--transaction_manager
+GRANT SELECT ON Transactionu TO tran_man;
+GRANT SELECT ON Subscriptionu TO tran_man;
+GRANT SELECT ON Shows TO tran_man;
+GRANT SELECT ON customer TO tran_man;
+GRANT EXECUTE ON handle_subscription_action TO tran_man;
+GRANT SELECT ON view_subscription_info TO tran_man;
+GRANT SELECT ON view_provider_annual_income TO tran_man;
+
+
+--Content Manager
+GRANT SELECT, INSERT, UPDATE, DELETE ON Actor TO cont_man;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Director TO cont_man;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Shows TO cont_man;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Show_Prov TO cont_man;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Direct TO cont_man;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Type TO cont_man;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Genre TO cont_man;
+GRANT SELECT ON Provider TO cont_man;
+GRANT SELECT ON Subscriptionu TO cont_man;
+GRANT EXECUTE ON show_management_pkg TO cont_man;
+GRANT SELECT ON regional_age_preferences TO cont_man;
+GRANT SELECT ON subscription_peak_periods_extended TO cont_man;
+*/
+
+
+
 --Function 1: check whether the table are exist or not;
 
 SET SERVEROUTPUT ON;
@@ -1097,7 +1143,7 @@ END;
 
 --UPDATE customer SET
 --    sub_status = CASE
---        WHEN sub_end_date IS NOT NULL AND sub_end_date < SYSDATE THEN 'Expired'
+--        WHEN sub_end_date IS NOT NULL AND sub_end_date <= SYSDATE THEN 'Expired'
 --        WHEN sub_end_date IS NULL THEN 'Unsubscribe'
 --        ELSE 'Subscribe'
 --    END;
@@ -1476,7 +1522,6 @@ END;
 BEGIN
     InsertActorRating(p_email => 'user2@exaple.com', p_actor_name => 'Aaron Paul', p_rating => 10);
 END;
--- **Error Handling**  If the rating is out of the range 1 to 5, it will raise error 'Rating must be between 1 and 5.' 
+-- *Error Handling*  If the rating is out of the range 1 to 5, it will raise error 'Rating must be between 1 and 5.' 
 -- And if null enter, it will raise error 'Email must not be null.'
 */
-
